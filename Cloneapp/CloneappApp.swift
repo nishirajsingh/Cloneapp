343ca522891877713d33b9f9f3076d7d316bc4a9
nishirajsingh <nishirajsingh2005@gmail.com>
2025-07-01 16:14:09 +0530

    Initial Commit

---
+++ Cloneapp/CloneappApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneappApp.swift
+//  Cloneapp
+//
+//  Created by student on 01/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CloneappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cloneapp/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  Cloneapp
+//
+//  Created by student on 01/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var inputText = ""
+    var body: some View {
+        VStack {
+            Image(.login)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200)
+            
+            ZStack{
+                Rectangle()
+                    .frame(width: 405,height: 800)
+                    .padding(0)
+                    .cornerRadius(30)
+                    .padding([.bottom], -250)
+                VStack{
+                    Text("Login")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .font(.system(size: 40))
+                    TextField("Enter Username",text: $inputText)
+                        .padding()
+                        .background(.white)
+                        .frame(width: 300,height: 50 )
+                        .cornerRadius(10)
+                    TextField("Enter Password",text: $inputText)
+                        .padding()
+                        .background(.white)
+                        .frame(width: 300,height: 50 )
+                        .cornerRadius(10)
+                    HStack {
+                        Button("Login In"){}
+                            .padding()
+                            .frame(width: 300,height: 50 )
+                            .background()
+                            .cornerRadius(20)
+                    }
+                }
+            }
+           
+        }
+        .padding(0)
+    }
+}
+
+#Preview {
+    ContentView()
+}
